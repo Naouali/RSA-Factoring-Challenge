89ad787574299360d142cd1e52c59b96435ef89c
naouali <naoualinabil@gmail.com>
2020-02-22 00:29:05 +0000

    Done!

--- fac.py
+++ fac.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
-def fac(number):	
+def fac(number):
+	i_list = []
 	for i in range(2,number):
-		for j in range (2,number):
-			if i * j == number:
-				k_list = []
-				m_list = []
-				k_list.append(i)
-				m_list.append(j)
-				k = max(k_list)
-				m = min(m_list)			
-	print("{:d} = {:d} * {:d}".format(number, k, m))
-	k_list.clear()
-	m_list.clear()
-
+		if number % i == 0:
+			break
+			i_list.append(i)
+	j = int(number / i)
+	print("{:d}={:d}*{:d}".format(number, j, i))
+	i_list.clear()
